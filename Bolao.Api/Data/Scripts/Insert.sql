c373ffdd62eee08278205a671849dd4516ef8ba6
Wellington Fernandes <wellington_fernands@yahoo.com.br>
2019-06-02 19:37:45 -0300

    .

--- Bolao.Api/Data/Scripts/Insert.sql
+++ Bolao.Api/Data/Scripts/Insert.sql
@@ -6,7 +6,6 @@ INSERT INTO typebet VALUES (1, 'Sena15Numbers', 'Descição');
 
 -- LOTTERY / LOTERRY NUMBER RESULT
 INSERT INTO lottery VALUES ('aefa86bb-f329-4f69-a024-b106a7b372e8', 17.50, NOW(), NOW(), DATE(NOW() + INTERVAL 3 DAY), NOW(), 1, 1);
-
 INSERT INTO lotterynumberresult VALUES (1, '01', 'aefa86bb-f329-4f69-a024-b106a7b372e8');
 INSERT INTO lotterynumberresult VALUES (2, '02', 'aefa86bb-f329-4f69-a024-b106a7b372e8');
 INSERT INTO lotterynumberresult VALUES (3, '03', 'aefa86bb-f329-4f69-a024-b106a7b372e8');
@@ -14,9 +13,7 @@ INSERT INTO lotterynumberresult VALUES (4, '04', 'aefa86bb-f329-4f69-a024-b106a7
 INSERT INTO lotterynumberresult VALUES (5, '05', 'aefa86bb-f329-4f69-a024-b106a7b372e8');
 INSERT INTO lotterynumberresult VALUES (6, '06', 'aefa86bb-f329-4f69-a024-b106a7b372e8');
 
--- LOTTERY / LOTERRY NUMBER RESULT
 INSERT INTO lottery VALUES ('5c42ca49-f3c8-4e5b-a72c-c46a871cd748', 20.10, NOW(), NOW(), DATE(NOW() + INTERVAL 7 DAY), NOW(), 1, 1);
-
 INSERT INTO lotterynumberresult VALUES (7, '07', '5c42ca49-f3c8-4e5b-a72c-c46a871cd748');
 INSERT INTO lotterynumberresult VALUES (8, '08', '5c42ca49-f3c8-4e5b-a72c-c46a871cd748');
 INSERT INTO lotterynumberresult VALUES (9, '09', '5c42ca49-f3c8-4e5b-a72c-c46a871cd748');
