5907980e41f1de8918b877e655fc98d483b28fa1
Wellington Fernandes <wellington_fernands@yahoo.com.br>
2019-09-17 22:59:31 -0300

    a

--- Bolao.Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
+++ Bolao.Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -1,7 +1,12 @@
-﻿namespace Bolao.Domain.Interfaces.UnitOfWork
+﻿using Bolao.Domain.Interfaces.Repositories;
+
+namespace Bolao.Domain.Interfaces.UnitOfWork
 {
     public interface IUnitOfWork
     {
+        IUserRepository UserRepository { get; }
+        IUserSecurityRepository UserSecurityRepository { get; }
+        ILotteryReposiory LotteryReposiory { get; }
         void Commit();
     }
 }
